f1d94cd3c71210833ebf8fda8cba700a0ce0163b
ssj <1029783071@qq.com>
2018-06-01 22:11:37 +0800

    fix train configure
    Signed-off-by: ssj <1029783071@qq.com>

--- config/config.py
+++ config/config.py
@@ -6,7 +6,7 @@ configure_names = ['init_test_mot16', 'init_test_mot17', 'init_train_mot17',
                    'exp_test_mot17_final_net', 'exp_train_mot17_final_net',
                    'init_train_mot17_final_net_lab', 'exp_test_mot17_final_net']
 
-current_select_configure = 'exp_test_mot17_final_net'
+current_select_configure = 'init_train_mot17'
 
 config = {
     'mot_root': r'/home/ssm/ssj/dataset/MOT17',
@@ -94,24 +94,29 @@ def init_test_mot17():
 all_functions += [init_test_mot17]
 
 def init_train_mot17():
+    config['epoch_size'] = 664
     config['mot_root'] = '/home/ssm/ssj/dataset/MOT17'
     config['base_net_folder'] = '/home/ssm/ssj/weights/MOT17/vgg16_reducedfc.pth'
-    config['log_folder'] = '/home/ssm/ssj/weights/MOT17/log0326-I50k-M80-G30-Continue0509'
-    config['save_folder'] = '/home/ssm/ssj/weights/MOT17/weights0326-I50k-M80-G30-Continue0509'
+    config['log_folder'] = '/home/ssm/ssj/weights/MOT17/0601-E120-M80-G30-log'
+    config['save_folder'] = '/home/ssm/ssj/weights/MOT17/0601-E120-M80-G30-weights'
+    config['save_images_folder'] = '/home/ssm/ssj/weights/MOT17/0601-E120-M80-G30-images'
     config['type'] = 'train'
     config['dataset_type'] = 'train'
-    config['resume'] = '/home/ssm/ssj/weights/MOT17/weights0326-I50k-M80-G30/ssj300_0712_80000.pth' #None
-    config['start_iter'] = 80000
-    config['cuda'] = True
-    config['batch_size'] = 8
-    config['num_workers'] = 16
-    config['iterations'] = 100050
-    config['learning_rate'] = 1e-2
-    config['false_constant'] = 10
+    config['resume'] = None
     config['detector'] = 'FRCNN'
-    config['max_object'] = 80
-    config['max_gap_frame'] = 30
+    config['start_iter'] = 0
+    config['iteration_epoch_num'] = 120
+    config['iterations'] = config['start_iter'] + config['epoch_size'] * config['iteration_epoch_num'] + 50
+    config['batch_size'] = 8
+    config['learning_rate'] = 1e-2
+    config['learning_rate_decay_by_epoch'] = (50, 80, 100, 110)
+    config['save_weight_every_epoch_num'] = 5
     config['min_gap_frame'] = 0
+    config['max_gap_frame'] = 30
+    config['false_constant'] = 10
+    config['num_workers'] = 16
+    config['cuda'] = True
+    config['max_object'] = 80
     config['min_visibility'] = 0.3
 
 
