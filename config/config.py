b685d87973dc322d72ee85b45b2fbc888caed5b1
shijieSun <shijieSun@chd.edu.cn>
2019-03-07 15:04:28 +0800

    change the batch size

--- config/config.py
+++ config/config.py
@@ -95,7 +95,7 @@ def init_train_mot17():
     config['start_iter'] = 0
     config['iteration_epoch_num'] = 120
     config['iterations'] = config['start_iter'] + config['epoch_size'] * config['iteration_epoch_num'] + 50
-    config['batch_size'] = 4
+    config['batch_size'] = 8
     config['learning_rate'] = 1e-2
     config['learning_rate_decay_by_epoch'] = (50, 80, 100, 110)
     config['save_weight_every_epoch_num'] = 5
